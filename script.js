05bb88d181b966ad0d505132b8a83b0a8b63ef68
OkieKenobi <travismartinez88@gmail.comgit config --global user.name Tmartinez0202>
2020-07-07 22:40:18 -0600

    added date and time to the search weather function

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ $(document).ready(function() {
     });
     $(".history").on("click", "li", function() {
         searchWeather($(this).text());
-
-
+        
+        
+        
     });
     
     function makeRow(text) {
-
+        
     }
-
+    
     function searchWeather(cityName) {
         $.ajax({
             type: "GET",
@@ -24,24 +25,33 @@ $(document).ready(function() {
                 //create history link for search
                 console.log(response);
                 $("#today").empty();
-                // var title = city name and current date
+                var title = $("<h1>").addClass("card-text").text(response.name);
+                var newDate = new Date(response.dt*1000)
+                var date = $("<h2>").addClass("card-text").text("Date: "+ newDate);
+
                 var card = $("<div>").addClass("card");
                 var wind = $("<p>").addClass("card-text").text("Wind speed: "+ response.wind.speed);
                 var temp = $("<p>").addClass("card-text").text("Temperature: " + response.main.temp);
                 var humid = $("<p>").addClass("card-text").text("Humidity: " + response.main.humidity);
                 var cardBody = $("<div>").addClass("card-body");
-                // var icon =
-                cardBody.append(wind, humid, temp)//put icon in here
+                var icon = $("<img>").attr({
+                    "src": "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png",
+                    "alt": response.weather[0].description,
+                    
+                })
+                
+                cardBody.append(title, date, wind, humid, temp, icon)
                 card.append(cardBody)
                 $("#today").append(card);
                 getForecast(cityName);
                 // function getUVIndex()//not city name
-
+                
+                
             }
-
+            
         });
     }
-
+    
     function getForecast(cityName) {
         $.ajax({
             type: "GET",
@@ -49,38 +59,44 @@ $(document).ready(function() {
             dataType: "JSON", 
             success: function(response) {
                 console.log(response);
-
+                
                 $("#forecast").html("<h4 class=\"mt3\">5 Day Forecast:</h4>").append("<div class=\"row\">");
-
+                
                 for (i = 0; i < response.list.length; i++){
                     if (response.list[i].dt_txt.indexOf("15:00:00")!== -1){
                         var col = $("<div>").addClass("col-md-2");
                         var card = $("<div>").addClass("card bg-primary text-white");
                         var body = $("<div>").addClass("card-body p-2");
-                        // var title = 
-                        // var icon =
+                        // var title = $("<h3>").addClass(card-text)("Day: "+ )
+                        var icon = $("<img>").attr({
+                            "src": "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png",
+                            "alt": response.list[0].weather[0].description,
+                            
+                        })
                         var temp = $("<p>").addClass("card-text").text("Temp: " + response.list[i].main.temp_max);
                         var humid = $("<p>").addClass("card-text").text("Humid: " + response.list[i].main.humidity);
-
-                        col.append(card.append(body.append(temp, humid)))//title and icon go here
+                        
+                        col.append(card.append(body.append(temp, humid, icon)))//title and icon go here
                         $("#forecast .row").append(col)
+                    }
                 }
-            }
-        }});
-    }
-
-    //getUVIndex function goes here
-
-
-    //get current history, if any
-    var history = JSON.parse(window.localStorage.getItem("history")) || [];
-
-    if (history.length > 0) {
-        searchWeather(history[history.length-1]);
-
-    }
-
-    for (var i = 0; i < history.length; i++) {
-        makeRow(history[i]);
-    }
-});
+            }});
+        }
+        
+        //getUVIndex function goes here
+        
+        
+        //get current history, if any
+        $(".list-group history").append(history);
+        var history = JSON.parse(window.localStorage.getItem("history")) || [];
+        
+        if (history.length > 0) {
+            searchWeather(history[history.length-1]);
+            
+        }
+        
+        for (var i = 0; i < history.length; i++) {
+            makeRow(history[i]);
+            
+        }
+    });
